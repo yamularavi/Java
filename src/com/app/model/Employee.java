3f8161f41c97e35c4a4b814fd4afcf021ccb2d97
MyPC <MyPC@DESKTOP-75PLD3I>
2019-01-03 10:36:34 -0800

    second file change

--- src/com/app/model/Employee.java
+++ src/com/app/model/Employee.java
@@ -3,5 +3,10 @@ package com.app.model;
 public class Employee {
 	private int id;
 	private String name;
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + "]";
+	}
+	
 
 }
